7dbd1ad1fab857481daa8b8cdaf1d1f672ffd8b1
KapsarovR <risto.kapsarov@gamail.com>
2023-03-26 13:10:13 +0200

    VS-19 install vue-router and  Home About and Login

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav>
+    <app-header />
+    <app-section-two :inputData="sectionTwo" />
+    <app-footer />
+  </nav>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppSectionTwo from "@/components/AppSectionTwo.vue";
+import AppFooter from "@/components/AppFooter.vue";
+export default {
+  name: "AboutPage",
+  components: {
+    AppHeader,
+    AppSectionTwo,
+    AppFooter,
+  },
+  data() {
+    return {
+      sectionTwo: [
+        {
+          title: "Monday",
+          description: "Lorem ipsum dolar sit amet",
+          image:
+            "https://funkylife.in/wp-content/uploads/2022/09/good-morning-image-from-funkylife-430.jpg",
+        },
+        {
+          title: "Tuesday",
+          description:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi",
+          image:
+            "https://funkylife.in/wp-content/uploads/2022/09/good-morning-image-from-funkylife-442.jpg",
+        },
+        {
+          title: "Wednesday",
+          description: "Lorem ipsum dolar sit amet",
+          image:
+            "https://funkylife.in/wp-content/uploads/2022/09/good-morning-image-from-funkylife-431.jpg",
+        },
+        {
+          title: "Thursday",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui omnis",
+          image:
+            "https://funkylife.in/wp-content/uploads/2022/08/good-morning-image-funkylife-467.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+    <app-header />
+    <app-section-five :inputData="sectionFive" />
+    <app-footer />
+  </nav>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppSectionFive from "@/components/AppSectionFive.vue";
+import AppFooter from "@/components/AppFooter.vue";
+export default {
+  components: {
+    AppHeader,
+    AppSectionFive,
+    AppFooter,
+  },
+  data() {
+    return {
+      sectionFive: [
+        {
+          message: "Enter your ",
+        },
+      ],
+    };
+  },
+};
+</script>
